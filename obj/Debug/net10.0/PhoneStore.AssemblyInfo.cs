8cce41b1200bf3a22d69aa9e768e092a2b952f60
batanhnhui-cloud <batanhnhui@gmail.com>
2026-05-18 11:02:52 +0700

    10

--- obj/Debug/net10.0/PhoneStore.AssemblyInfo.cs
+++ obj/Debug/net10.0/PhoneStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhoneStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67fbac298dbe71a058a19f678296fa6bfe9c7aae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1400c20eb8311b8520671d0c28b3ec9072a92a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhoneStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhoneStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
